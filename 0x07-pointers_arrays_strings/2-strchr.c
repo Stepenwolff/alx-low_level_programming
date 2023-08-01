4f7ecc7d2e4af3bac21d1dabaf2428eb9638b8c3
Stepenwolff <stephensuscartel@gmail.com>
2023-08-01 09:39:54 +0300

    EM pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,4 @@
-#include "main.h"
+i#include "main.h"
 
 /**
  * _strchr - Locate the first occurrence of a character in a string.
@@ -10,12 +10,12 @@
  */
 char *_strchr(char *s, char c)
 {
-    int i = 0;
+	int i = 0;
 
-    for (; s[i] >= '\0'; i++)
-    {
-        if (s[i] == c)
-            return (&s[i]);
-    }
-    return (0);
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		return (&s[i]);
+	}
+	return (0);
 }
