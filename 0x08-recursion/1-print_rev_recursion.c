f63a101e96e77863bd5730ce2b5a6a41b7f97765
Stepenwolff <stephensuscartel@gmail.com>
2023-08-02 12:09:14 -0700

    recursions

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * reverse_print - Prints a string in reverse using recursion.
- * @s: The string to be printed in reverse.
+ * _print_rev_recursion - Prints  reverse of a string.
+ * @s: The string to be printed.
  */
-void reverse_print(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0') /* Base case: end of the string */
+	if (*s == '\0') /* Base case: end of string */
 		return;
 
-	reverse_print(s + 1); /* Recursive call with the next character */
-	_putchar(*s); /* Print the current character */
+	_print_rev_recursion(s + 1); /* Recursive call with the next character */
+	_putchar(*s); /* Print current character */
 }
--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -42,6 +42,6 @@ int check_palindrome(char *s, int i, int len)
 	if (*(s + i) != *(s + len - 1))
 	return (0);
 	if (i >= len)
-        return (1);
+	return (1);
 	return (check_palindrome(s, i + 1, len - 1));
 }
